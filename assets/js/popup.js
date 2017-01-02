aa1bd8ef7cb2017828994cbb3adc42a3a87a2190
Dmitry Borzyonok <dima.matters@gmail.com>
2017-01-02 02:34:22 +0200

    Fast parsing exchange rate from NBU

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,14 +1,13 @@
 (function () {
     document.addEventListener('DOMContentLoaded', function () {
-
-        var $date = document.getElementById('date');
+        const $date = document.getElementById('date');
 
         function replaceTranslations() {
-            var $localizeItems = document.querySelectorAll(
+            const $localizeItems = document.querySelectorAll(
                 '[data-localize], [data-value-localize]'
             );
             for (let $item of $localizeItems) {
-                let data = $item.dataset;
+                const data = $item.dataset;
                 if (data.valueLocalize) {
                     $item.value = chrome.i18n.getMessage(data.valueLocalize);
                 }
@@ -19,7 +18,7 @@
         }
 
         function setDefaultValueToDate() {
-            let currentDate = new Date();
+            const currentDate = new Date();
             $date.value = currentDate.getFullYear() + '-' + (currentDate.getMonth() + 1);
         }
 
@@ -37,25 +36,21 @@
 
         function requestNBU(date) {
             attachThrobber('#avg-nbu');
-            let dateString = date[1] + '.' + date[0];
-            let url = 'http://www.bank.gov.ua/control/uk/curmetal/' +
+            const dateString = date[1] + '.' + date[0];
+            const url = 'http://www.bank.gov.ua/control/uk/curmetal/' +
                 'currency/search?formType=searchPeriodForm&time_step=daily&' +
-                'currency=169&outer=table&' +
+                'currency=169&outer=xml&' +
                 'periodStartTime=01.' + dateString +
                 '&periodEndTime=20.' + dateString;
             fetch(url).then(function (response) {
                 return response.text();
             }).then(function (data) {
-                let html = document.createElement('div');
-                html.innerHTML = data;
-                var sum = 0;
-                let $rows = html
-                    .querySelectorAll('.content > table')[3]
-                    .querySelectorAll('tbody tr')[1]
-                    .querySelectorAll('td table tbody tr');
+                const parser = new DOMParser();
+                data = parser.parseFromString(data, 'text/xml');
+                const $rows = data.querySelectorAll('exchange_rate');
+                let sum = 0;
                 for (let $row of $rows) {
-                    let $rowTds = $row.querySelectorAll('td');
-                    sum += parseFloat($rowTds[$rowTds.length - 1].textContent.trim());
+                    sum += parseFloat($row.textContent.trim());
                 }
                 attachRate('#avg-nbu', ((sum / $rows.length) / 100).toFixed(2));
             });
@@ -69,17 +64,16 @@
             i = i || 1;
             counter = counter || 0;
             if (i > 20) {
-                console.log('pb ' + sum + ' rows ' + counter);
                 attachRate('#avg-pb', (sum / counter).toFixed(2));
                 return;
             }
-            var placeDate = i + '.' + date[1] + '.' + date[0];
+            const placeDate = i + '.' + date[1] + '.' + date[0];
             fetch('https://api.privatbank.ua/p24api/exchange_rates?json&date=' + placeDate)
                 .then(function (response) {
                     return response.json();
                 }).then(function (data) {
                 if (data.exchangeRate.length) {
-                    var item = data.exchangeRate.find(function (item) {
+                    const item = data.exchangeRate.find(function (item) {
                         return item.currency === 'USD';
                     });
                     sum += item.saleRateNB;
@@ -93,7 +87,7 @@
         function attachFormSubmitHandler() {
             document.forms[0].addEventListener('submit', function (e) {
                 e.preventDefault();
-                var date = $date.value.split('-');
+                const date = $date.value.split('-');
                 requestNBU(date);
                 requestPrivatbank(date);
             });
